689e81da70eaf4b36c482227ad3ab12c05c8f358
lalitnankar <83298104+lalitnankar@users.noreply.github.com>
2022-04-30 23:51:16 +0530

    Add files via upload

---
+++ Task3/app.js
@@ -0,0 +1,11 @@
+const celsius = document.querySelector('#celsius');
+const fahrenheit = document.querySelector('#fahrenheit');
+
+celsius.oninput = () => {
+  let output = (parseFloat(celsius.value) * 9) / 5 + 32;
+  fahrenheit.value = parseFloat(output.toFixed(2));
+};
+fahrenheit.oninput = () => {
+  let output = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+  celsius.value = parseFloat(output.toFixed(2));
+};
